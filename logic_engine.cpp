4d9a90df586995316fa46e37f68f6e4d0799716c
Ameer Masoud <ameer.masoud@gmail.com>
2024-12-31 22:30:18 -0600

    added logic_engine.cpp to the repo for future use in calculating the evaluation and validation of blueprints (speed optimization)

---
+++ logic_engine.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Blueprint {
+
+    Blueprint from_json() {} 
+    //TODO: Figure out how python and c++ will communicate blueprints with each other (e.g. json)
+
+};
+
+
+class LogicEngine {
+
+    bool validateBlueprint(Blueprint& self) {}
+    vector<bool> evaluateBlueprint(Blueprint& self) {}
+
+    //TODO: implement these two functions
+
+};
